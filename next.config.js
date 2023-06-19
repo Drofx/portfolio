d80f2afb389b2a1f2debebee2538f8a126573534
Drofx <drofxyt@gmail.com>
2023-06-19 18:31:15 -0300

    newsoruce

--- next.config.js
+++ next.config.js
@@ -1,26 +1,12 @@
   /** 
    *@type {import('next').NextConfig} 
    */
-   const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-   let assetPrefix = ''
-   let basePath = '/'
-   
-   if (isGithubActions) {
-     // trim off `<owner>/`
-     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-   
-     assetPrefix = `/${repo}/`
-     basePath = `/${repo}`
-   }
 
   const nextConfig ={  
-    output: 'export',
     images: {
       domains: ['raw.githubusercontent.com'],
     },
-    assetPrefix:assetPrefix,
-    basePath:basePath,
+    basePath:"/portfolio",
   }
 
   module.exports = nextConfig
